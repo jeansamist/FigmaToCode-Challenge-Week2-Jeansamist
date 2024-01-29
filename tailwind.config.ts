6a7ea93ccd7ffbf02eea229b38973970263285da
BAHA Ephraim <jeansamist@gmail.com>
2024-01-29 18:44:31 +0100

    Adding images and resources

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,6 +3,14 @@ import type { Config } from "tailwindcss";
 const config: Config = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+        xl: "80%",
+      },
+    },
     extend: {},
   },
   plugins: [],
